ff2ccddedf5eb0fb1d4075b1b26eb5f9e10e3b31
Prem <premkhodke.99@gmail.com>
2024-11-17 01:32:57 +0530

    tabs

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <main>
+    <TabNav :tabs="['Home', 'Settings', 'Profile']" :selected="selected" @selected="setSelected">
+      <Tab :isSelected="selected === 'Home'">
+        <p>Some test text</p>
+      </Tab>
+      <Tab :isSelected="selected === 'Settings'">
+        <h1>More test text</h1>
+      </Tab>
+      <Tab :isSelected="selected === 'Profile'">
+        <ul>
+          <li>list 1</li>
+          <li>list 2</li>
+          <li>list 3</li>
+        </ul>
+      </Tab>
+    </TabNav>
+  </main>
+</template>
+
+<script>
+import TabNav from './components/TabNav.vue';
+import Tab from './components/Tab.vue';
+
+export default {
+  name: 'App',
+  components: { TabNav, Tab },
+  data() {
+    return {
+      selected: 'Home'
+    }
+  },
+  methods: {
+    setSelected(tab) {
+      this.selected = tab;
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/TabNav.vue
@@ -0,0 +1,60 @@
+<template>
+    <div >
+        <ul class="nav nav-tabs">
+            <li v-for="tab in tabs" :key="tab" class="nav-item">
+                <a href="javascript:void(0)" class="nav-link" :class="{ active: tab === selected }"
+                    @click="setTab(tab)">
+                    {{ tab }}
+                </a>
+            </li>
+        </ul>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tabs: {
+            type: Array,
+            required: true,
+        },
+        selected: {
+            type: String,
+            required: true,
+        }
+    },
+    methods: {
+        setTab(tab) {
+            this.$emit('selected', tab);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.nav-tabs {
+    display: flex;
+}
+
+.nav-item {
+    margin-right: 10px;
+}
+
+.nav-link {
+    padding: 10px;
+    text-decoration: none;
+    border: 1px solid transparent;
+    cursor: pointer;
+    color: #007bff;
+}
+
+.nav-link.active{
+    border-bottom: 1px solid white;
+}
+
+ul{
+    list-style: none;
+    margin-bottom: 10px;
+}
+</style>
